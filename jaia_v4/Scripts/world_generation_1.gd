04c809ac84a76e49caedc77d277641fe22b25fc6
bloo <nexushellstein@gmail.com>
2025-08-08 08:02:07 +0800

    worked on procedural generation

---
+++ jaia_v4/Scripts/world_generation_1.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var generate: Button = $CanvasLayer/MarginContainer2/Generate
+@onready var gaea_generator: GaeaGenerator = $GaeaGenerator
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_generate_pressed() -> void:
+	gaea_generator.generate()

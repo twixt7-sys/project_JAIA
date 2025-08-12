a2d8fbaae061aad899e2e38e1ec99494a8df5dc6
bloo <nexushellstein@gmail.com>
2025-08-12 10:10:25 +0800

    Isolated background from titlescreen

--- jaia_v5/Scenes/game.gd
+++ jaia_v5/Scenes/game.gd
@@ -1,14 +1,25 @@
 extends Node2D
 
 @onready var title_screen: TitleScreen = preload("res://Scenes/title_screen.tscn").instantiate()
+@onready var animation_player: AnimationPlayer = $Control/AnimationPlayer
+@onready var fade_out: AnimationPlayer = $"Control/fade out"
+@onready var contents: Node2D = $Control/Contents
 
 func _ready() -> void:
-	add_child(title_screen)
-	title_screen.start_game.connect(start_game)
-	title_screen.open_settings.connect(open_settings)
+	animation_player.play("bg animation")
+	add_title_screen()
+	fade_out.play("fade_out")
 	
-func start_game() -> void: print("game starts")
-func open_settings() -> void: print("settings opened")
+func start_game() -> void: 
+	print("game starts")
+
+func open_settings() -> void:
+	print("settings opened")
 
 func _process(delta: float) -> void:
 	pass 
+
+func add_title_screen() -> void:
+	contents.add_child(title_screen)
+	title_screen.start_game.connect(start_game)
+	title_screen.open_settings.connect(open_settings)
--- jaia_v5/Scripts/title_screen.gd
+++ jaia_v5/Scripts/title_screen.gd
@@ -2,22 +2,11 @@ extends Control
 
 class_name TitleScreen
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var fade_out: AnimationPlayer = $"fade out"
-
 signal start_game
 signal open_settings
-
-func _ready() -> void:
-	animation_player.play("bg animation")
-	fade_out.play("fade_out")
-
-func _process(delta: float) -> void:
-	pass
 
 func _on_play_pressed() -> void: 
 	emit_signal("start_game")
 
 func _on_settings_pressed() -> void:
 	emit_signal("open_settings")
-	

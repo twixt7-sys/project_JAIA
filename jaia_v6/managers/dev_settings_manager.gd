06a8b544083b8d9954552638dbff42529a871d53
bloo <nexushellstein@gmail.com>
2025-09-20 13:57:41 +0800

    reset button developed

--- jaia_v6/managers/dev_settings_manager.gd
+++ jaia_v6/managers/dev_settings_manager.gd
@@ -1,6 +1,8 @@
 @tool
 extends Node
 
+# [ VAL, DEFAULT, MIN, MAX ]
+
 @onready var settings: Dictionary = {
 	"Player": {
 		"core": {
@@ -16,14 +18,14 @@ extends Node
 			"attack": { "power": 0.0, "speed": 0.0, "crit_chance": 0.0 },
 			"defense": { "armor": 0.0, "resistance": 0.0, "dodge": 0.0 },
 			"movement": {
-				"weight": 0.0,
-				"speed": 500.0,
-				"sprint speed": 500.0,
-				"friction": 0.95,
-				"dash_speed": 500.0,
-				"dash_duration": 0.25,
-				"dash_cooldown": 0.25,
-			},
+				  "weight": { "current": 0.0, "default": 1.0, "min": 0.0, "max": 100.0 },
+				  "speed": { "current": 500.0, "default": 1.0, "min": 0.0, "max": 10000.0 },
+				  "sprint speed": { "current": 500.0, "default": 500.0, "min": 0.0, "max": 50000.0 },
+				  "friction": { "current": 0.95, "default": 0.95, "min": 0.0, "max": 1.0 },
+				  "dash_speed": { "current": 500.0, "default": 500.0, "min": 0.0, "max": 50000.0 },
+				  "dash_duration": { "current": 0.25, "default": 0.25, "min": 0.0, "max": 1.0 },
+				  "dash_cooldown": { "current": 0.25, "default": 0.25, "min": 0.0, "max": 10.0 }
+			}
 		},
 	},
 	"World": {
--- jaia_v6/scenes/ui/gui/windows/dev_window.gd
+++ jaia_v6/scenes/ui/gui/windows/dev_window.gd
@@ -1,7 +1,7 @@
 @tool
 extends Control
 
-@onready var setting_items: VBoxContainer = $"MarginContainer/Panel/VBoxContainer/Left Navbar/Settings Body/Body Panel/Panel/MarginContainer/VBoxContainer/Panel2/MarginContainer/ScrollContainer/Setting Items"
+@onready var setting_items: VBoxContainer = $"MarginContainer/Panel/VBoxContainer/Left Navbar/Settings Body/Body Panel/Panel/Sub-setting/VBoxContainer/Body/MarginContainer/ScrollContainer/Setting Items"
 
 @onready var settings: Dictionary = DevSettingsManager.settings
 @onready var player_settings: Dictionary = settings["Player"]
@@ -22,10 +22,10 @@ func _refresh_settings() -> void:
 
 func _populate_settings() -> void:
 	for key in player_movement.keys():
-		var value = player_movement[key]
+		var value = player_movement[key]["current"]
 		var s_item: SettingItem = SETTING_ITEM_SCENE.instantiate()
 		s_item.settingName = key
-		s_item.slider_value = float(value)
+		s_item.slider_value = value
 		s_item.initial_current = str(value)
 		setting_items.add_child(s_item)
 		print(value)
@@ -43,5 +43,15 @@ func _on_save_pressed() -> void:
 	var keys := player_movement.keys()
 	for i in keys.size():
 		var child: SettingItem = setting_items.get_child(i)
-		player_movement[keys[i]] = child.slider_value
+		player_movement[keys[i]]["current"] = child.slider_value
 		print("global variable ", keys[i], " set to ", child.slider_value)
+
+
+func _on_reset_pressed() -> void:
+	var i = 0
+	for key in player_movement.keys():
+		var value = player_movement[key]["default"]
+		var s: SettingItem = setting_items.get_children()[i]
+		i += 1
+		s.slider_value = value
+		s.text_value = str(s.slider_value)
